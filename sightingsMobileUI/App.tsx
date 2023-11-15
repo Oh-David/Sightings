0a4f0f5b18ff6fae8b21530782c7002d17f032f1
David Oh <DavidOh@protonmail.com>
2023-11-14 20:53:47 -0800

    Create users via UI and Subscribe to AWS

--- sightingsMobileUI/App.tsx
+++ sightingsMobileUI/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import Amplify from 'aws-amplify';
-// import awsConfig from './aws-exports';
+import { Amplify } from 'aws-amplify';
 import awsConfig from './src/aws-exports';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from './src/screens/SignInScreen/SignInScreen';
 import CreateAccountScreen from './src/screens/CreateAccount/CreateAccountScreen';
 
-Amplify.Amplify.configure(awsConfig);
+Amplify.configure(awsConfig);
 
 const Stack = createNativeStackNavigator();
 
--- sightingsMobileUI/src/screens/CreateAccount/CreateAccountScreen.tsx
+++ sightingsMobileUI/src/screens/CreateAccount/CreateAccountScreen.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Auth } from 'aws-amplify';
 import { View, TextInput, Button, Text, StyleSheet, TouchableOpacity  } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
@@ -13,23 +14,67 @@ type SignUpScreenProps = {
   navigation: SignUpScreenNavigationProp;
 };
 
+
+
 const CreateAccountScreen: React.FC<SignUpScreenProps> = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async () => {
+    try {
+      const signUpResponse = await Auth.signUp({
+        username: email,
+        password,
+        attributes: {
+          email,
+          phone_number: phoneNumber,
+        }
+      });
+      console.log("Sign up success", signUpResponse);
+    } catch (error) {
+      console.log("Sign up error", error);
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <TextInput 
-        placeholder="Full Name" 
+      {/* <TextInput 
+        value={phoneNumber}
+        placeholder="Phone Number" 
+        onChangeText={setPhoneNumber}
         style={styles.input} 
-        testID="input-full-name" 
-      />
+        testID="input-phone-number" 
+        keyboardType='phone-pad'
+      /> */}
       <TextInput 
+        value={email}
         placeholder="Email" 
+        onChangeText={setEmail}
         style={styles.input} 
+        testID="email-address"
         keyboardType="email-address"
-        testID="input-email" 
+        autoCapitalize="none"
       />
+      <TextInput 
+        value={phoneNumber}
+        placeholder="Phone Number" 
+        onChangeText={setPhoneNumber}
+        style={styles.input} 
+        keyboardType="phone-pad"
+        testID="input-number" 
+      />
+      {/* <TextInput 
+        value={phoneNumber}
+        placeholder="PhoneNumber" 
+        onChangeText={setPhoneNumber}
+        style={styles.input} 
+        testID="input-phone-number" 
+        keyboardType='phone-pad'
+      /> */}
       <TextInput 
         placeholder="Password" 
+        onChangeText={setPassword}
         style={styles.input} 
         secureTextEntry 
         testID="input-password" 
@@ -41,8 +86,8 @@ const CreateAccountScreen: React.FC<SignUpScreenProps> = ({ navigation }) => {
         testID="input-confirm-password" 
       />
       <Button 
-        title="Create Account" 
-        onPress={() => { /* Handle account creation */ }} 
+        title="Sign Up" 
+        onPress={handleSignUp} 
       />
 
       <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
