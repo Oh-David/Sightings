75fb58bfaccd238ebc6064036126889b8a84028c
David Oh <DavidOh@protonmail.com>
2023-11-25 21:00:05 -0800

    Map Icons and Pinpoints

--- sightingsMobileUI/src/screens/LandingPage/Features/Map/MapComponent.tsx
+++ sightingsMobileUI/src/screens/LandingPage/Features/Map/MapComponent.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import redMarker from '../../../../assets/images/redpp.png';
+import blueMarker from '../../../../assets/images/bluepp.png';
+import bugMarker from '../../../../assets/images/bugpp.png';
 
 interface Sighting {
     id: number;
@@ -13,19 +16,31 @@ interface Sighting {
   interface WildlifeMapProps {
     sightings: Sighting[];
   }
-  
+
   const WildlifeMap: React.FC<WildlifeMapProps> = ({ sightings }) => {  
+    let markerImage: any;
+    var x = 3;
+    if (x === 1) {
+      markerImage = redMarker;
+    } else if (x === 2) {
+      markerImage = blueMarker;
+    } else if (x === 3) {
+      markerImage = bugMarker;
+    }
+
     return (
       <View style={{ flex: 1, width: '100%' }}>
         <View style={{ flex: 1, width: '100%' }}>
-          <MapView style={{ flex: 1, width: '100%' }}>
+        <MapView style={{ flex: 1, width: '100%' }}>
           {sightings.map(sighting => (
-          <Marker
-            key={sighting.id}
-            coordinate={{ latitude: sighting.latitude, longitude: sighting.longitude }}
-            title={sighting.title}
-            description={sighting.description}
-          />
+            
+            <Marker
+              key={sighting.id}
+              coordinate={{ latitude: sighting.latitude, longitude: sighting.longitude }}
+              title={sighting.title}
+              description={sighting.description}
+              image={markerImage} 
+            />
           ))}
         </MapView>
       </View>
