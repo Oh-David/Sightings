5641404a2241f26e994ac5825f764f098e23ea5e
InnovateWithRK <rk.go@icloud.com>
2024-07-11 17:57:48 -0700

    refactor: Update stored procedures to fetch products by owner

--- sightingsMobileUI/src/screens/Data/Sql/V1MockStoredProcedures.sql
+++ sightingsMobileUI/src/screens/Data/Sql/V1MockStoredProcedures.sql
@@ -4,7 +4,6 @@ AS
 BEGIN
     SELECT * FROM Users WHERE id = @UserId
 END;
-GO;
 
 CREATE OR ALTER PROCEDURE GetProductsByOwner
     @OwnerId VARCHAR(255)
@@ -38,7 +37,6 @@ BEGIN
     FROM ProductCategories
     ORDER BY category;
 END;
-GO;
 
 CREATE OR ALTER PROCEDURE GetProductsNotOwnedByUser
     @OwnerId VARCHAR(255)
@@ -67,4 +65,45 @@ BEGIN
     WHERE 
         p.ownerId <> @OwnerId;
 END;
-GO;
+
+CREATE OR ALTER PROCEDURE AddBid
+    @product1Id VARCHAR(255),
+    @product2Id VARCHAR(255)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO Bids (product1Id, product2Id)
+    VALUES (@product1Id, @product2Id);
+
+    SELECT SCOPE_IDENTITY() AS NewBidId;
+END;
+
+CREATE OR ALTER PROCEDURE RemoveBid
+    @bidId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DELETE FROM Bids
+    WHERE id = @bidId;
+END;
+
+CREATE OR ALTER PROCEDURE GetAllBids
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT
+        B.id AS bidId,
+        B.product1Id,
+        P1.name AS product1Name,
+        B.product2Id,
+        P2.name AS product2Name
+    FROM
+        Bids B
+    JOIN
+        Products P1 ON B.product1Id = P1.id
+    JOIN
+        Products P2 ON B.product2Id = P2.id;
+END;
